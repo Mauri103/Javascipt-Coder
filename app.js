a02ef0f9b384bffe6837290153c627f2dc7d165c
Mauricio Alvarez <maurialvarez103@gmail.com>
2023-02-06 19:19:42 -0300

    agrego metodo reduce

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 totalBalance = 0;
-totalIngresos = 0;
 totalEgresos = 0;
 
 let egresos = [];
-const ingresos = [];
+let ingresos = [];
 
-
-total = document.getElementById("balanceTotal").innerHTML = "$ 0"; 
-ingr = document.getElementById("totalIngresos").innerHTML = "$ 0";
-egr = document.getElementById("totalEgresos").innerHTML = "$ 0";
+restablecerValores();
 
 function Ingreso(descripcion, valor){
     this.descripcion = descripcion;
@@ -26,7 +22,7 @@ function realizarConteo(){
     if(tipo === "Ingreso"){
         calcularIngresos();
         ingreso = new Ingreso(obtenerDescripcion(), obtenerValorIngresado());
-        agregarIngreso();
+        agregarIngreso();     
 
             
     }else{
@@ -35,21 +31,22 @@ function realizarConteo(){
         agregarEgreso();
     } 
     
-
+    let totalIngresos = ingresos.reduce((contador, array) => contador + array.valor, 0);
+    let totalEgresos = egresos.reduce((contador, array) => contador + array.valor, 0);
+    
     total = document.getElementById("balanceTotal").innerHTML = "$ " + totalBalance;
     ingr = document.getElementById("totalIngresos").innerHTML = "$ " + totalIngresos;
     egr = document.getElementById("totalEgresos").innerHTML = "$ " + totalEgresos;
 }
+
+
+
 function calcularIngresos(){
     ingreso = obtenerValorIngresado();
-    totalIngresos = totalIngresos + ingreso;
-    totalBalance = totalBalance + ingreso;
 }
 
 function calcularEgresos(){
     egreso = obtenerValorIngresado();
-    totalEgresos = totalEgresos + egreso;
-    totalBalance = totalBalance - egreso;
 }
 function obtenerValorIngresado(){
     valor = parseFloat(document.getElementById("valorIngresado").value);
@@ -69,6 +66,12 @@ function agregarIngreso(){
 function agregarEgreso(){
     egresos.push(egreso);
     console.log(egresos);
+}
+
+function restablecerValores(){
+    total = document.getElementById("balanceTotal").innerHTML = "$ 0"; 
+    ingr = document.getElementById("totalIngresos").innerHTML = "$ 0";
+    egr = document.getElementById("totalEgresos").innerHTML = "$ 0";
 }
 
 
